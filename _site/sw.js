fdae875a273b312a70cbd332182028e33ffd13c5
Áleson Medeiros <aleson.medeiros@gmail.com>
2020-12-20 12:47:26 -0300

    Forestry

--- _site/sw.js
+++ _site/sw.js
@@ -1,10 +1,10 @@
-var CACHE_NAME = "saikiransripada-20201220111423";
+var CACHE_NAME = "saikiransripada-20201220124024";
 
 self.addEventListener("install", function(e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll([
-        "/css/style.css?202012201114",
+        "/css/style.css?202012201240",
         "/"
       ]);
     })
